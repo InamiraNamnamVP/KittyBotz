5b101926a33e2570a5eba344e3c3c05b7dab2131
MinariBotz <81842252+InamiraNamnamVP@users.noreply.github.com>
2021-04-07 15:16:42 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,14 @@
+const donasi = () => { 
+	return `       
+┌──「 *DONASI BOT* 」
+│
+├◪ *DONASI BISA MELALUI :*
+├◪ *BIMA THREE : 08990542731*
+├◪ *DANA : 08990542731*
+├◪ *OVO : 08990542731
+├◪ *LINK AJA : 08990542731
+│
+└──「 *KittyBotz* 」`
+}
+
+exports.donasi = donasi
